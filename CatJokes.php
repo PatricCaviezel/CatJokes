fba0e574278fe54621c7ff1b6330517b2a4b2714
Patric Caviezel <patric.caviezel@gmail.com>
2021-10-21 15:33:36 +0200

    Neue styles hinzugefügt

--- CatJokes.php
+++ CatJokes.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
 
-      <head>
-<!Author 1: Jon Landa <jonlanda06@gmail.com>
-<!Author 2: Patric Caviezel <patric.caviezel@gmail.com>
-<!Author 3: Luc Hauser <luc.hauser@bluewin.ch>
+  <head>
+    <!Author 1: Jon Landa <jonlanda06@gmail.com>
+    <!Author 2: Patric Caviezel <patric.caviezel@gmail.com>
+    <!Author 3: Luc Hauser <luc.hauser@bluewin.ch>
 
-<link rel="stylesheet" href="Styles.css">
-
-<meta name="viewport" content="width=device-width,height=device-heigt, initial-scale=1.0">
-        <title>CatJokes</title>
-
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/3468/3468377.png">
+    <link rel="stylesheet" href="Styles.css">
+    <meta name="viewport" content="width=device-width,height=device-heigt, initial-scale=1.0">
+      <title>
+        CatJokes
+      </title>
         <?php
         function get_img_data()
         {
@@ -40,6 +41,14 @@
                 if ($counter > 2) {
                     array_push($img_data, $elem);
                 }
+            }
+            if (count($img_data) > 3) {
+                unset($img_data[0]);
+                $fixed_array = [];
+                foreach ($img_data as $elem) {
+                    array_push($fixed_array, $elem);
+                }
+                $img_data = $fixed_array;
             }
             $max_size = 600;
             if ($max_size < $img_data[1]) {
@@ -85,35 +94,80 @@
                 echo 'error ' . $code;
             }
         }
+
+        $url = "https://uselessfacts.jsph.pl/random.json?language=en";
+
+        $headers = array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+        );
+
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response = curl_exec($curl);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        if ($code == 200) {
+            $response = json_decode($response, true);
+        } else {
+            echo 'error ' . $code;
+        }
+
+        $print = $response['text'] . PHP_EOL . "Source: " . $response['source'];
         ?>
 
-      </head>
-      <body class="rainbow">
-      <center>
-      <p style="font-size:70px;">CatJokes</p>
-      </center>
-      <div id="background-log">
-  <div class="button-login">
+  </head>
+  <body class="rainbow">
     <center>
-    <button onClick="window.location.reload();" class="refresh" style="font-size: 40px">refresh</button>
-      </center>
-  </div>
-</div>
+      <p class="fontcool">
+        <i>
+          CatJokes
+        </i>
+      </p>
+    </center>
+    <div id="background-log">
+      <div class="button-login">
+        <center>
+          <button onClick="window.location.reload();" class="refresh">
+            refresh
+          </button>
+        </center>
+      </div>
+    </div>
       <center>
         <img src="<?php echo $img_url; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
       </center>
-        <center>
-          <p class="size">
+      <center>
+        <h1>
+          After this purrfect image, here's a random fact.
+        </h2>
+        <p class="size">
+          <?php
+            print $print;
+            ?>
+        </p>
+      </center>
+      <center>
+        <h1>
+          in case you didn't like the fact, here's a joke ;).
+        </h2>
+      </center>
+      <center>
+        <p class="size">
         <?php
         print joke();
         ?>
         </p>
         </center>
-      </body>
-      <footer>
-        <center>
-        <h4 style="color:red">Sometimes there is an error. Just refresh to fix it</h4>
-        </center>
-      </footer>
-
+  </body>
+  <footer>
+  <a href="https://github.com/PatricCaviezel/CatJokes"><img src="https://icdn.enterinit.com/wp-content/uploads/2017/05/26081202/github-logo.png" alt="Git Hub Link"  style="width:96px;height:48px;"></a>
+  </footer>
 </html>
